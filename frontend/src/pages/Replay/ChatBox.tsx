07273061031e83cb912fa61c5ff14cbfde5624b7
xtu1111 <xtu@tohmatsu.co.jp>
2025-09-10 11:18:09 +0000

    feat: update marketing workflows and frontend

--- frontend/src/pages/Replay/ChatBox.tsx
+++ frontend/src/pages/Replay/ChatBox.tsx
@@ -380,7 +380,6 @@ export const RightPanel = observer(() => {
                                     displaylogo: false,
                                     modeBarButtonsToRemove: ['lasso2d', 'select2d']
                                 }}
-                                style={{ width: '100%', height: 200 }}
                             />
                         </div>
                     );
